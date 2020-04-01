ce368f29eb4797e97177ea8b4aac656f14a6e564
join <join.eu.pn@gmail.com>
2020-04-01 09:40:37 +0300

    Comment initial implementation

--- app-from-scratch/src/App.js
+++ app-from-scratch/src/App.js
@@ -5,6 +5,28 @@ function Welcome(props) {
 	return <h3> Hello, {props.name}</h3>
 }
 
+function Comment(props) {
+	return (
+		<div className="Comment">
+			<div className="UserInfo">
+				<img className="Avatar" 
+					src={props.author.avatarUrl} 
+					alt={props.author.name} 
+				/>
+				<div className="UserInfo-name">
+					{props.author.name}
+				</div>
+			</div>
+			<div className="Comment-text">
+				{props.text}
+			</div>
+			<div className="Comment-date">
+				{props.date}
+			</div>
+		</div>
+	);
+}
+
 function App() {
 	return (
 		<div>
