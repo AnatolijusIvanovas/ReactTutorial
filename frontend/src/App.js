6eabcf6bf57c9822ead1b9518c5e6fff8b48680b
join <join.eu.pn@gmail.com>
2020-04-02 21:02:31 +0300

    data implemented as App class state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const data = [
-	{
-		title: 'Work 1',
-		priority: 1,
-	},
-	{
-		title: 'Study 2',
-		priority: 10,
-	},
-	{
-		title: 'Sleep 3',
-		priority: -10,
-	}
-];
-
 class App extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			 data: [
+				{
+					title: 'Work 1',
+					priority: 1,
+				},
+				{
+					title: 'Study 2',
+					priority: 10,
+				},
+				{
+					title: 'Sleep 3',
+					priority: -10,
+				}
+			]
+		}
+	}
+	
 	render() {
 		return (
 			<div className="App">
-				{data.map(item =>
+				{this.state.data.map(item =>
 					<div>
 						{item.title}
 					</div>
